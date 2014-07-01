c5589fcba4b5f0cbcec7c9a11857ee10ef4c558c
Mark Roberts <markandrusroberts@gmail.com>
2014-06-30 20:16:56 -0700

    Pushing some broken tests; will fix

--- test/Test.hs
+++ test/Test.hs
@@ -1,12 +1,30 @@
 module Main where
 
+import Twilio.Types
+
+import Twilio.Accounts          as Accounts
+import Twilio.Applications      as Applications
+import Twilio.Calls             as Calls
+import Twilio.ConnectApps       as ConnectApps
+import Twilio.Messages          as Messages
+import Twilio.OutgoingCallerIDs as OutgoingCallerIDs
+import Twilio.PhoneNumbers      as PhoneNumbers
+import Twilio.UsageRecords      as UsageRecords
+
+import Control.Monad (sequence_)
 import Control.Monad.IO.Class (liftIO)
 import System.Environment (getEnv)
-import Twilio.Calls as Calls
-import Twilio.Types
 
 -- | Print calls.
 main :: IO ()
 main = runTwilio' (getEnv "ACCOUNT_SID")
                   (getEnv "AUTH_TOKEN")
-     $ Calls.get >>= liftIO . print
+     $ sequence_
+       [ Accounts.get          >>= liftIO . print
+       , Applications.get      >>= liftIO . print
+       , Calls.get             >>= liftIO . print
+       , ConnectApps.get       >>= liftIO . print
+       , Messages.get          >>= liftIO . print
+       , OutgoingCallerIDs.get >>= liftIO . print
+       , PhoneNumbers.get      >>= liftIO . print
+       , UsageRecords.get      >>= liftIO . print ]
