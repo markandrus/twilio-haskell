50944811a57cc007eef7a8ffb14cc0f842ef911d
Steven Leiva <leiva.steven@gmail.com>
2017-10-20 15:27:53 -0400

    Add QuickCheck.

--- test/unit/Twilio/Types/PriceUnitSpec.hs
+++ test/unit/Twilio/Types/PriceUnitSpec.hs
@@ -1,9 +1,12 @@
 {-#LANGUAGE OverloadedStrings #-}
+{-#LANGUAGE InstanceSigs #-}
 module Twilio.Types.PriceUnitSpec where
 
 import Test.Hspec
 import Twilio.Types.PriceUnit
 import Data.Aeson
+import Test.QuickCheck hiding (Success)
+import Data.Text (Text, pack)
 
 main :: IO ()
 main = hspec spec
@@ -18,6 +21,14 @@ spec = do
         fromJSON (String "EUR") `shouldBe` (Success EUR)
       it "should work for JPY" $ do
         fromJSON (String "JPY") `shouldBe` (Success JPY)
-      it "should work for arbitrary string" $ do
+      it "should work for BTC" $ do
         let expectedResults = (Success $ OtherPriceUnit "BTC")
         fromJSON (String "BTC") `shouldBe` expectedResults
+      it "should work for arbitrary strings" $ property $
+        \code -> fromJSON (String code) == (Success $ OtherPriceUnit code)
+
+instance Arbitrary Text where
+  arbitrary :: Gen Text
+  arbitrary = do
+      string <- arbitrary :: Gen String -- generate arbitrary string
+      return $ pack string -- turn it into a text
